820b0c43cac4bea6b2bcf8fb9245ecc017535cd7
redraw <agustinbv@gmail.com>
2021-03-20 02:43:37 -0300

    tv

---
+++ src/pages/Tv.vue
@@ -0,0 +1,101 @@
+<template>
+  <Layout :show-footer="false" :show-header="false">
+    <a href="#" class="logo" @click="next">
+      <g-image src="/logo-tierradelfuego-glitch.gif"></g-image>
+    </a>
+    <div class="overlay"></div>
+    <div id="tv-player"></div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      player: null,
+    }
+  },
+
+  metaInfo () {
+    return {
+      script: [
+        { src: "https://www.youtube.com/iframe_api", id: "youtube-api" }
+      ],
+    }
+  },
+
+  async mounted () {
+    window.addEventListener("load", () => {
+      this.player = new YT.Player('tv-player', {
+        playerVars: {
+          controls: 0,
+          rel: 0,
+          showinfo: 0,
+          enablejsapi: 1,
+          autoplay: 1,
+          modestbranding: 1,
+          origin: window.location.host,
+        },
+        events: {
+          'onReady': this.onPlayerReady,
+          'onStateChange': this.onPlayerStateChange
+        }
+      })
+    })
+
+  },
+
+  methods: {
+    onPlayerReady(event) {
+      const player = event.target
+      player.cuePlaylist({
+        list: this.$settings.tv_playlist,
+        listType: "playlist"
+      })
+    },
+
+    onPlayerStateChange(event) {
+      const player = event.target
+      if (event.data === YT.PlayerState.CUED) {
+        player.setShuffle(true)
+        setTimeout(() => {
+          player.playVideoAt(0);
+        }, 500) // YT bug fix
+      }
+    },
+
+    next() {
+      this.player.nextVideo()
+    }
+  }
+}
+</script>
+
+<style>
+#tv-player {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  transform: scale(1.25);
+}
+.logo {
+  position: fixed;
+  top: 2em;
+  left: 2em;
+  width: 100px;
+  max-width: 25vw;
+  z-index: 12;
+}
+.overlay {
+  position: fixed;
+  z-index: 10;
+  background: black;
+  opacity: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
